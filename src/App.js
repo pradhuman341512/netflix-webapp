034c31d4c1adbdfe672630528c4401a807020a8f
Pradhuman <singhpradhuman077@gmail.com>
2024-10-28 09:54:49 +0530

    Normalize line endings

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p className= 'h-20 w-20 bg-red-900 text-black'>Hello</p>
+    </div>
+  );
+}
+
+export default App;
